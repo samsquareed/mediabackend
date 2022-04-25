8af6967925bd7b8c6fe5671517b671f9acfeac4d
samsquareed <samsquare.professional@gmail.com>
2022-04-25 14:56:41 +0530

    did this ...(spreading) stuff own and i completely understood that

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,19 @@
 const router = require('express').Router();
 const User = require("../models/User")  // as soon as i imported Model here the model is created in mongodb 
+const bcrypt = require('bcrypt')
 
 //register user :
 router.post("/register", async (req,res)=>{
-
-    const user = new User(req.body)
-    
-    await user.save()
-    res.send("ok")
+    try{
+        const salt = await bcrypt.genSalt(10);
+        const hashedpassword = await bcrypt.hash(req.body.password, salt);
+        const newUser = new User({...req.body, password : hashedpassword});
+        // newUser.password = hashedpassword
+        const user = await newUser.save();
+        res.status(200).json(user);
+    } catch(err){
+        console.error(err);
+    }
 })
 
 module.exports = router
