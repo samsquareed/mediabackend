e5a8fded7bdaf04a314da147afc6533bfcd56b56
samsquareed <samsquare.professional@gmail.com>
2022-04-25 12:57:49 +0530

    mongoose is added

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ const dotenv = require('dotenv')
 const helmet = require('helmet')
 const morgan = require('morgan')
 const app = express()
-
 dotenv.config();
 
-const PORT = 5000 || process.env.PORT
+mongoose.connect(process.env.MONGO_URL, ()=>{
+    console.log("connected to mongoDb");
+})
 
+
+//middlewares
+app.use(express.json())
+app.use(helmet())
+app.use(morgan("common"))
+
+
+
+const PORT = 5000 || process.env.PORT
 app.listen(PORT, ()=>{
     console.log(`listening on ${PORT}`);
 })
